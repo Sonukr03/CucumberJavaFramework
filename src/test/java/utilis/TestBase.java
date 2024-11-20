8ff5fcf7701ada877f3ca0d8adfbae0b1432a90b
Arnu <arnu@Arnus-MacBook-Air.local>
2024-11-20 16:28:39 +0530

    added browser stack function

--- src/test/java/utilis/TestBase.java
+++ src/test/java/utilis/TestBase.java
@@ -2,12 +2,16 @@ package utilis;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestBase {
 
@@ -32,9 +36,41 @@ public class TestBase {
 
 			if (browser.equalsIgnoreCase("chrome")) {
 
-				System.setProperty("webdriver.chrome,driver",
-						System.getProperty("user.dir") + "/src/test/resources/chromedriver");
-				driver = new ChromeDriver();
+				//System.setProperty("webdriver.chrome,driver",
+				//		System.getProperty("user.dir") + "/src/test/resources/chromedriver");
+				
+				
+				// Set W3C-compliant capabilities
+		        MutableCapabilities capabilities = new MutableCapabilities();
+
+		        // BrowserStack-specific settings
+		        MutableCapabilities browserstackOptions = new MutableCapabilities();
+		        browserstackOptions.setCapability("os", "Windows");
+		        browserstackOptions.setCapability("osVersion", "11");
+		        browserstackOptions.setCapability("resolution", "1920x1080");
+		        browserstackOptions.setCapability("browserVersion", "latest");
+		        browserstackOptions.setCapability("projectName", "BrowserStack Integration");
+		        browserstackOptions.setCapability("buildName", "Build-1");
+		        browserstackOptions.setCapability("sessionName", "Sample Test");
+
+		        // Add browserstackOptions to main capabilities
+		        capabilities.setCapability("bstack:options", browserstackOptions);
+		        capabilities.setCapability("browserName", "Chrome");
+
+			        // Replace USERNAME and ACCESS_KEY with your BrowserStack credentials
+			        String username = "sonukumar_fumScC";
+			        String accessKey = "Hqx7bCuZf9krCQabWJhq";
+			        
+			    String URll =    "https://sonukumar_fumScC:Hqx7bCuZf9krCQabWJhq@hub-cloud.browserstack.com/wd/hub";
+
+			//        URL browserStackUrl = new URL("https://" + username + ":" + accessKey + "@hub-cloud.browserstack.com/wd/hub");
+
+			        driver = new RemoteWebDriver(new URL(URll), capabilities);
+			       // driver.get("https://www.example.com");
+				
+				
+				//-------------=================---------------===========-------------
+				//driver = new ChromeDriver();
 			}
 
 			else if (browser.equalsIgnoreCase("firefox")) {
